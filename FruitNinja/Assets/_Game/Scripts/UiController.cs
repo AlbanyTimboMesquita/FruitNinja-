0193c7de7065cd62c7aa1a29b4877fff6fec876b
Albany Timbó Mesquita <4lb4ny@gmail.com>
2024-02-22 17:29:42 -0300

    Programação da interface de usuario do game

---
+++ FruitNinja/Assets/_Game/Scripts/UiController.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class UiController : MonoBehaviour
+{
+    public TMP_Text txtScore,txtHighscore;
+    public Image[] imgLives;
+    public Button btnPause,btnResume,btnMainMenu,btnClosePauseMenu,btnSounds;
+    public GameObject panelGame,panelPause;
+    void Start()
+    {
+        panelGame.gameObject.SetActive(true);
+        panelPause.gameObject.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void ButtonPauseGame(){
+        panelGame.gameObject.SetActive(false);
+        panelPause.gameObject.SetActive(true);
+        Time.timeScale=0f;//pausando o jogo
+    }
+
+    public void ButtonClosePanelPause(){
+        panelGame.gameObject.SetActive(true);
+        panelPause.gameObject.SetActive(false);
+        Time.timeScale=1f;//despausando o jogo
+    }
+}
