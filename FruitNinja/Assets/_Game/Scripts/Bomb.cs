06979dd3f72a4024c74480ad2ccb38d6520ea6bd
Albany Timbó Mesquita <4lb4ny@gmail.com>
2024-02-23 11:01:08 -0300

    Animacao  bomb

---
+++ FruitNinja/Assets/_Game/Scripts/Bomb.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bomb : MonoBehaviour
+{
+    [SerializeField] private float speed;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       Rotate(); 
+    }
+    private void Rotate(){
+        transform.Rotate(new Vector3(0f,speed,0f) * Time.deltaTime);
+    }
+}
